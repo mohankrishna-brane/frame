36c043623bfef9f64a21c295bc61e313377c5a4b
mohankrishna-brane <mohankrishna@braneenterprises.com>
2026-03-11 19:55:32 +0530

    increased resolution

--- enroll_ui.py
+++ enroll_ui.py
@@ -409,8 +409,8 @@ elif st.session_state.step == "capture":
             async_processing=True,
             media_stream_constraints={
                 "video": {
-                    "width":     {"ideal": 640, "max": 640},
-                    "height":    {"ideal": 480, "max": 480},
+                    "width":     {"ideal": 1280, "max": 1280},
+                    "height":    {"ideal": 720, "max": 720},
                     "frameRate": {"ideal": 15,  "max": 20},
                 },
                 "audio": False,
